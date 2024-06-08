9955153be99e391e416cea342be609d55d1e1d29
Jona Zwetsloot <info@jonazwetsloot.nl>
2024-06-08 19:29:45 +0200

    Updated Firefox extension to v3.7
    Updated the Firefox extension to the newest version.

--- Firefox/popup.js
+++ Firefox/popup.js
@@ -5,11 +5,10 @@ chrome.storage.local.get(["enabled"]).then((result) => {
     updateCheckbox();
 });
 document.getElementById('reset-all').addEventListener('click', function() { if (confirm("Wil je echt alle opslagdata wissen?")) { chrome.storage.local.clear(); chrome.storage.local.set({ enabled: true }); somtodayEnabled = true; updateCheckbox(); } });
-document.getElementById('enable-disable').addEventListener('click', function() { somtodayEnabled = !somtodayEnabled; updateCheckbox(); });
+document.getElementById('enable-disable').addEventListener('click', function() { somtodayEnabled = !somtodayEnabled; updateCheckbox(); chrome.storage.local.set({enabled:somtodayEnabled}); });
 function updateCheckbox() {
     document.getElementById('icon-checked').style.display = somtodayEnabled ? 'inline-block' : 'none';
     document.getElementById('icon-unchecked').style.display = somtodayEnabled ? 'none' : 'inline-block';
-    chrome.storage.local.set({enabled:somtodayEnabled})
 }
 document.getElementById('txt-enable-disable').innerHTML = chrome.i18n.getMessage("enableDisable");
 document.getElementById('txt-to-somtoday').innerHTML = chrome.i18n.getMessage("toSomtoday");
