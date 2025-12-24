c10075be74d82727e78cf72beae8b9b73b5e9eee
Jona Zwetsloot <info@jonazwetsloot.nl>
2025-12-24 14:45:06 +0100

    Missing semicolon
    A missing semicolon broke the Android build again... I really need to be more careful while writing code.

--- Chromium/scripts/main_functions.js
+++ Chromium/scripts/main_functions.js
@@ -1017,7 +1017,7 @@ function onload() {
                 const parent = document.createElement('div');
                 parent.id = 'mod-new-year';
                 const div = document.createElement('div');
-                div.id = 'mod-new-year-fireworks'
+                div.id = 'mod-new-year-fireworks';
                 parent.appendChild(div);
                 const button = document.createElement('button');
                 button.innerHTML = '&times;';
@@ -1655,7 +1655,7 @@ function onload() {
                 svg = 'M64 390.3L153.5 256 64 121.7V390.3zM102.5 448H281.5L192 313.7 102.5 448zm128-192L320 390.3V121.7L230.5 256zM281.5 64H102.5L192 198.3 281.5 64zM0 48C0 21.5 21.5 0 48 0H336c26.5 0 48 21.5 48 48V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V48z';
         }
         return '<svg ' + start + classname + 'height="1em" viewBox="' + viewbox + '"><path fill-rule="evenodd" ' + (n(color) ? '' : 'fill="' + color + '" ') + 'd="' + svg + '"/></svg>';
-    };
+    }
 
     // Show a message on top of the page. Similair to browser confirm().
     function modMessage(title, description, link1, link2, red1, red2, noBackgroundClick) {
